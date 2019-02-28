1c5939758f9167ed8a3b08635cf300fef7a12471
SamBritt <sam.britt2@yahoo.com>
2019-02-28 14:29:54 -0600

    finished practice problems for string concatentation

---
+++ overly-excited.js
@@ -0,0 +1,33 @@
+// Create an array that contains the words in the sentence
+let sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"];
+
+
+function addExcitement(theWordArray, punct, num) {
+
+    // Each time the for loop executes, you're going to add one more word to this string
+    let buildMeUp = ""
+
+    for (let i = 0; i < theWordArray.length; i++) {
+        // Concatenate the new word onto buildMeUp
+
+        // Print buildMeUp to the console
+        if ((i + 1) % 3 === 0) {
+            buildMeUp += ' ' + theWordArray[i] + punct.repeat(num);
+            console.log(buildMeUp);
+      
+        } else {
+            buildMeUp += ' ' + theWordArray[i];
+            console.log(buildMeUp);
+        
+        }
+
+        // buildMeUp += ' ' + theWordArray[i];
+
+        // if((i + 1) % 3 === 0){
+        //     buildMeUp += '!';
+        // }
+        // console.log(buildMeUp);
+    }
+}
+
+addExcitement(sentence, '?', 2);
